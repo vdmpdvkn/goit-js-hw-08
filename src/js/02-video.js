9b20c3f390ea5c03e134e909f7443bdb252cd8f5
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-03-01 03:01:40 +0300

    vimeo, feedback finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new vimeo(iframeEl);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(setTimeStamp, 1000));
+
+let parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+player.setCurrentTime(parsedData);
+
+function setTimeStamp(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
+
+window.addEventListener('keydown', evt => {
+  if (evt.key === 'Space') {
+    player.pause();
+  }
+});
+
+// мне кажется мой способ каким-то читерским, слишком мало кода как то)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const { email, message } = formEl.elements;
+
+localStorageCheck();
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  if (email.value.trim() !== '' || message.value.trim() !== '') {
+    const parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(parsedData);
+  } else {
+    alert('Введите валидные данные');
+  }
+  evt.currentTarget.reset();
+}
+
+function onFormInput(evt) {
+  if (evt.target.name === 'email') {
+    evt.target.value = evt.target.value.trim();
+  }
+
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({ email: email.value.trim(), message: message.value.trim() })
+  );
+}
+function localStorageCheck() {
+  const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedData) {
+    email.value = JSON.parse(savedData).email;
+    message.value = JSON.parse(savedData).message;
+  }
+}
