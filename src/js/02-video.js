6ebf20ea33d1a33996ad81855b3acd661ad33cb4
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-03-01 04:00:32 +0300

    noo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,17 +8,9 @@ const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 setVideoTiming();
 
 player.on('timeupdate', throttle(setTimeStamp, 1000));
-player.on('play', onSpacePause);
 
 function setTimeStamp(data) {
   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
-}
-function onSpacePause() {
-  window.addEventListener(evt => {
-    if (evt.code === 'Space') {
-      player.pause();
-    }
-  });
 }
 function setVideoTiming() {
   const parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
