21911d8e9be7268d358d2f64def94ecc64b9094a
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-03-01 03:37:24 +0300

    throw parse into a function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,20 +5,24 @@ const iframeEl = document.querySelector('#vimeo-player');
 const player = new vimeo(iframeEl);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
+setVideoTiming();
+
 player.on('timeupdate', throttle(setTimeStamp, 1000));
 player.on('play', onSpacePause);
-
-let parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-player.setCurrentTime(parsedData);
 
 function setTimeStamp(data) {
   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
 }
-
 function onSpacePause() {
   window.addEventListener(evt => {
     if (evt.code === 'Space') {
       player.pause();
     }
   });
+}
+function setVideoTiming() {
+  const parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (parsedData) {
+    player.setCurrentTime(parsedData);
+  }
 }
